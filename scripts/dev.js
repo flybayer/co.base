7e2deae6f1ede5c3d5034fabd2c8397c9a03cf65
Eric Vicenti <eric@aven.io>
2020-04-25 23:34:04 -0700

    🎮 Experimental support for electron

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,10 +1,12 @@
 const pathJoin = require('path').join;
-const fs = require('fs');
+const fs = require('fs-extra');
 const spawn = require('@expo/spawn-async');
 const appName = process.argv[2];
 const srcDir = pathJoin(__dirname, '../src');
 const appConfigPath = appName && pathJoin(srcDir, appName, 'app.json');
 const appSrcDir = appName && pathJoin(srcDir, appName);
+const buildDir = pathJoin(__dirname, '../build');
+const appBuildDir = pathJoin(buildDir, appName);
 
 const appConfig = appConfigPath && JSON.parse(fs.readFileSync(appConfigPath));
 
@@ -35,26 +37,69 @@ async function maybeStartDevServer() {
   }
 }
 
+const APP_TYPES = {
+  node: {
+    runDevApp: async function runDevApp() {
+      spawn(
+        pathJoin(__dirname, '../node_modules/.bin/babel-node'),
+        [pathJoin(appSrcDir, appConfig.serverEntry)],
+        {
+          stdio: 'inherit',
+          env: {
+            ...process.env,
+            BABEL_ENV: 'web-development',
+            NODE_ENV: 'development',
+          },
+        },
+      );
+      console.log(
+        `ℹ️ Starting server at: ${pathJoin(appSrcDir, appConfig.serverEntry)}`,
+      );
+    },
+  },
+  electron: {
+    runDevApp: async function runDevApp() {
+      await spawn(
+        pathJoin(__dirname, '../node_modules/.bin/babel'),
+        ['--copy-files', '-d', pathJoin(appBuildDir, 'dist'), 'src'],
+        {
+          stdio: 'inherit',
+          env: { ...process.env, NODE_ENV: 'web-production' },
+        },
+      );
+      await fs.writeFile(
+        pathJoin(appBuildDir, 'dist', appName, 'package.json'),
+        JSON.stringify(
+          {
+            main: appConfig.electronEntry,
+          },
+          null,
+          2,
+        ),
+      );
+      await spawn(
+        pathJoin(__dirname, '../node_modules/.bin/electron'),
+        [pathJoin(appBuildDir, 'dist', appName)],
+        {
+          stdio: 'inherit',
+        },
+      );
+    },
+  },
+};
+
+async function runMainApp() {
+  const appType = appConfig.type || 'node';
+  const appTypeDef = APP_TYPES[appType];
+  return appTypeDef.runDevApp();
+}
+
 async function run() {
   if (!appName) {
     return startDevServer();
   }
   await maybeStartDevServer();
-  console.log(
-    `ℹ️ Starting server at: ${pathJoin(appSrcDir, appConfig.serverEntry)}`,
-  );
-  spawn(
-    pathJoin(__dirname, '../node_modules/.bin/babel-node'),
-    [pathJoin(appSrcDir, appConfig.serverEntry)],
-    {
-      stdio: 'inherit',
-      env: {
-        ...process.env,
-        BABEL_ENV: 'web-development',
-        NODE_ENV: 'development',
-      },
-    },
-  );
+  await runMainApp();
 }
 
 run().catch(err => {
---
+++ src/aven/electron/index.js
@@ -0,0 +1,1 @@
+export { default as mountElectronApp } from './mountElectronApp';
