fc27e99aab5756342428633bac02553232418f80
Eric Vicenti <eric@aven.io>
2020-05-22 18:26:48 -0700

    📋 SmallForm in plane UI lib

--- src/aven/auth/createAuthNavigator.js
+++ src/aven/auth/createAuthNavigator.js
@@ -1,52 +1,10 @@
 import React from 'react';
-import { View, Text } from '@rn';
+import { Text } from '@rn';
 import { createFullscreenSwitchNavigator } from '@aven/navigation-web';
 import { useCloudClient, useStream } from '@aven/cloud';
 import { useFocus, useNavigation } from '@aven/navigation-hooks';
 import { Button, Stack, TextInput } from '@aven/plane';
-import { Link } from '@aven/navigation-web';
-import { useTheme } from '@aven/plane';
-
-function SmallForm({ children, title }) {
-  const { borderRadius, paddingVertical, paddingHorizontal } = useTheme();
-  return (
-    <View
-      style={{
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignSelf: 'stretch',
-      }}
-    >
-      <View
-        style={{
-          flex: 1,
-          maxWidth: 320,
-          borderWidth: 1,
-          borderColor: '#ccc',
-          borderRadius,
-          paddingHorizontal,
-          paddingVertical,
-          paddingTop: 42,
-        }}
-      >
-        <Text
-          style={{
-            position: 'absolute',
-            top: -6,
-            left: paddingHorizontal,
-            fontSize: 28,
-            fontFamily: 'Helvetica',
-            fontWeight: 'bold',
-            color: '#bbb',
-          }}
-        >
-          {title}
-        </Text>
-        {children}
-      </View>
-    </View>
-  );
-}
+import { SmallForm } from '@aven/plane';
 
 export default function createAuthNavigator(PageWrapper) {
   function VerifyCodeForm({ onSubmit, onCancel }) {
@@ -266,7 +224,6 @@ export default function createAuthNavigator(PageWrapper) {
     const { navigate } = useNavigation();
     const clientState = useStream(client.clientState);
     React.useEffect(() => {
-      console.log('zoomg', clientState);
       if (clientState?.session) {
         navigate('AuthAccount');
       } else if (clientState !== undefined) {
@@ -283,7 +240,6 @@ export default function createAuthNavigator(PageWrapper) {
     const client = useCloudClient();
     const { navigate } = useNavigation();
     const clientState = useStream(client.clientState);
-    console.log('clientState', clientState);
     React.useEffect(() => {
       if (clientState !== undefined && !clientState?.session) {
         navigate('AuthLogin');
