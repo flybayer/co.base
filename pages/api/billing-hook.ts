7143e017d2437656ed892f83d17b8821c03826fd
Eric Vicenti <eric@aven.io>
2020-08-21 13:53:52 -0700

    repair stripe webhooks

--- pages/api/billing-hook.ts
+++ pages/api/billing-hook.ts
@@ -1,24 +1,44 @@
 import { readSignedWebhook } from "../../api-utils/stripe";
 import { NextApiRequest, NextApiResponse } from "next";
-import { sendEmail } from "../../api-utils/email";
 import bodyParser from "body-parser";
+import { apiRespond } from "../../api-utils/apiRespond";
+import { Error400 } from "../../api-utils/Errors";
+import notifyAdministrator from "../../api-utils/notifyAdministrator";
+
+const eventTypeHandlers: Record<string, (event: any) => any> = {
+  "customer.created": (event: any) => {
+    console.log("Customer created:");
+    return {};
+  },
+};
 
 async function handleAction(req: NextApiRequest, res: NextApiResponse) {
   const signature = req.headers["stripe-signature"];
   const event = readSignedWebhook(req.body, signature);
-  await sendEmail(
-    "eric@aven.io",
-    "WebHook Test",
-    JSON.stringify(event, null, 2)
-  );
-  return {};
+
+  if (event.object !== "event") {
+    throw new Error400({ message: 'Expected an object of type "event"' });
+  }
+  if (eventTypeHandlers[event.type]) {
+    await eventTypeHandlers[event.type](event);
+  } else {
+    console.log("Unhandled stripe event: ", event.type);
+    console.log(JSON.stringify(event));
+    await notifyAdministrator(
+      "Unknown Stripe Webhook",
+      JSON.stringify(event, null, 2)
+    );
+  }
+  return {
+    message: "Thank you, Stripe.",
+  };
 }
 
 const reader = bodyParser.raw({ type: "application/json" });
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   reader(req, res, () => {
-    handleAction(req, res).catch((err) => console.error(err));
+    apiRespond(res, handleAction(req, res));
   });
 };
 
