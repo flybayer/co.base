54317dace73041f2c40777305547acb85ae02a7d
Eric Vicenti <eric@aven.io>
2020-08-27 10:51:15 -0700

    cleanup node modules

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,10 @@ cd ..
 # The workflow has already pulled, but just to be sure..
 git pull
 
-# Get all deps. Note: Sometimes there is a build error and node_modules needs to be wiped and replaced
+# Sometimes there is a build error and node_modules needs to be wiped and replaced
+rm -rf node_modules
+
+# Get all deps.
 yarn
 
 # Runs `next build`, as defined in /package.json[scripts][build]
