6ce5b2e6fbb47b6780f049c558a25bc73ff79c4c
Eric Vicenti <eric@aven.io>
2020-11-24 23:37:07 -0800

    buildfix

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 import Link from "next/link";
 import { InnerWidth } from "./CommonViews";
 import AvenLogo from "./AvenLogo";
+import { Button } from "@chakra-ui/core";
 
 const HeaderContainer = styled.div`
   background: #0e2b49;
@@ -26,14 +27,18 @@ export default function SiteHeader() {
   return (
     <HeaderContainer>
       <InnerWidth>
-        <Link href="/">
-          <div style={{ cursor: "pointer" }}>
-            <AvenLogo />
-          </div>
-        </Link>
         <span>
+          <Link href="/">
+            <div style={{ cursor: "pointer" }}>
+              <AvenLogo />
+            </div>
+          </Link>
           <HeaderLink href="/docs">Docs</HeaderLink>
           <HeaderLink href="/pricing">Pricing</HeaderLink>
+        </span>
+        <span>
+          <HeaderLink href="/login">Log In</HeaderLink>
+          <Button colorScheme="blue">Create a Site</Button>
         </span>
       </InnerWidth>
     </HeaderContainer>
