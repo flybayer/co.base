cf14aeba65c8cccac63aad908b5f3a6016dfc806
Eric Vicenti <eric@aven.io>
2020-12-10 16:48:59 -0800

    port move from 9000

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,15 +1,19 @@
+import { NextApiResponse } from "next";
 import { ReactElement } from "react";
 
 function Error({ statusCode }: { statusCode?: number }): ReactElement {
-  return <p>{statusCode ? `An error ${statusCode} occurred on server` : "An error occurred on client"}</p>;
+  console.log("eee");
+  return <p>Ok, error accurerd</p>;
+  // return <p>{statusCode ? `An error ${statusCode} occurred on server` : "An error occurred on client"}</p>;
 }
 
-Error.getInitialProps = ({ res, err }: { res: any; err: any }) => {
-  console.log("-ayao----------");
-  console.log({ res, err });
-  console.log("-----------");
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
+Error.getInitialProps = ({ res, err }: { res: NextApiResponse; err: any }) => {
+  // console.log("-ayao----------");
+  // console.log({ res, err });
+  // console.log("-----------");
+  // const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  // return { statusCode };
+  return {};
 };
 
 export default Error;
