7b76f5b6fa419e82c0cdff2d00149ecb3f965098
Eric Vicenti <ericvicenti@gmail.com>
2021-03-14 14:28:03 -0700

    move legacy. blitz starter app

---
+++ legacy/README.md
@@ -0,0 +1,25 @@
+# [Aven Cloud](https://aven.io) • [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) [![Aven Tests](https://github.com/AvenCloud/cloud/workflows/Test%20Suite/badge.svg)](https://github.com/AvenCloud/cloud/actions?query=workflow%3A%22Test+Suite%22) [![Pull Requests Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=square)](https://aven.io/docs/contributing)
+
+The main Aven Cloud repository, containing the code and content of the [site an aven.io](https://aven.io).
+
+## [How to run](https://aven.io/docs/self-hosting-guide)
+
+To run this website on your computer, [follow this guide](https://aven.io/docs/self-hosting-guide).
+
+### [Twitter](https://twitter.com/aven_cloud)
+
+Follow and tweet at [@aven_cloud](https://twitter.com/aven_cloud) or [@ericvicenti](https://twitter.com/ericvicenti).
+
+### [Discord](https://discord.gg/XvhnTHJ)
+
+You're welcome to [join us here on Discord](https://discord.gg/XvhnTHJ).
+
+### [Contributors Guide](https://aven.io/docs/contributing)
+
+### [Code of Conduct](https://aven.io/code-of-conduct)
+
+All channels will be moderated and strictly enforced according to our [Code of Conduct](https://aven.io/code-of-conduct).
+
+### [License](https://github.com/AvenCloud/sky/blob/main/LICENSE.md)
+
+Licensed under [Apache 2.0](https://github.com/AvenCloud/cloud/blob/main/LICENSE.md)
---
+++ legacy/lib/server/server.ts
@@ -0,0 +1,191 @@
+import { Response } from "express";
+import { createServer } from "http";
+import * as dotenv from "dotenv";
+import next from "next";
+import { parse } from "url";
+import WebSocket from "ws";
+import express from "express";
+import cookieParser from "cookie-parser";
+import { testOutput } from "./TestOutput";
+import spawnAsync from "@expo/spawn-async";
+import { database } from "../data/database";
+import { flushEvents, SERVER_HOST, writeEvent } from "../data/HostEvent";
+import { databaseUrl } from "./config";
+import {
+  clearHandler,
+  connectNotifications,
+  disconnectNotifications,
+  subscribeNotifications,
+  unsubscribeNotifications,
+} from "./DataNotify";
+
+dotenv.config();
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+
+console.log("Starting Host " + SERVER_HOST);
+
+const defaultPort = dev ? 3001 : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : defaultPort;
+
+let closeServer: null | (() => Promise<void>) = null;
+
+const sockets = new Map<number, WebSocket>();
+
+async function startServer() {
+  await connectNotifications();
+  const server = express();
+  const handle = app.getRequestHandler();
+  server.use(
+    express.static(__dirname + "/public", {
+      maxAge: "30d",
+    }),
+  );
+  server.use(cookieParser());
+  server.use((req: any, res: Response) => {
+    const parsedUrl = parse(req.url, true);
+    return handle(req, res, parsedUrl);
+  });
+  const httpServer = createServer(server);
+
+  const heartbeatInterval = setInterval(() => {
+    writeEvent("Heartbeat", { socketCount: sockets.size });
+  }, 30_000);
+
+  closeServer = async () => {
+    // fixme: todo: websocket shutdown /disconnections?
+    await new Promise<void>((resolve, reject) => {
+      clearInterval(heartbeatInterval);
+      console.log("Closing HTTP server.");
+      httpServer.close();
+      const rejectTimeout = setTimeout(() => {
+        console.log("HTTP server did not close within 10 seconds!");
+        reject(new Error("HTTPCloseError"));
+      }, 10_000);
+      httpServer.on("close", () => {
+        console.log("HTTP server did close.");
+        clearTimeout(rejectTimeout);
+        resolve();
+      });
+    });
+  };
+  const wss = new WebSocket.Server({
+    server: httpServer,
+  });
+  await new Promise<void>((resolve, reject) => {
+    httpServer.listen(port, () => {
+      resolve();
+    });
+    httpServer.on("error", reject);
+  });
+
+  writeEvent("HostOpen", undefined);
+
+  testOutput({ type: "ServerReady", port });
+  dev && console.log(`> Ready on http://localhost:${port}`);
+
+  let clientIdCount = 0;
+
+  function clientSend(clientId: number, data: any) {
+    const socket = sockets.get(clientId);
+    if (!socket) {
+      throw new Error("no socket with this client id");
+    }
+    socket.send(JSON.stringify(data));
+  }
+
+  function handleSubscribe(clientId: number, siteName: string, nodeKey: string) {
+    subscribeNotifications(`${siteName}/${nodeKey}`, clientId, (message) => {
+      console.log("subscribe notify!!", clientId, message);
+      clientSend(clientId, message);
+    });
+  }
+
+  function handleUnsubscribe(clientId: number, siteName: string, nodeKey: string) {
+    unsubscribeNotifications(`${siteName}/${nodeKey}`, clientId);
+  }
+
+  function handleMessage(clientId: number, message: any) {
+    if (message.t === "sub") return handleSubscribe(clientId, message.siteName, message.nodeKey);
+    if (message.t === "unsub") return handleUnsubscribe(clientId, message.siteName, message.nodeKey);
+  }
+
+  wss.on("connection", (socket: WebSocket) => {
+    const clientId = clientIdCount++;
+    sockets.set(clientId, socket);
+    writeEvent("SocketConnect", { clientId });
+    clientSend(clientId, {
+      t: "info",
+      clientId,
+      host: SERVER_HOST,
+    });
+    socket.on("message", (data: string) => {
+      const msg = JSON.parse(data);
+      handleMessage(clientId, msg);
+    });
+    socket.on("close", () => {
+      sockets.delete(clientId);
+      clearHandler(clientId);
+      writeEvent("SocketDisconnect", { clientId });
+    });
+  });
+}
+
+async function prepareDockerDev() {
+  if (!dev) return;
+  if (process.env.SKIP_DOCKER) return;
+  console.log("Docker startup..");
+  await spawnAsync("docker-compose", ["-f", "../../docker-compose.yml", "up", "-d"], {
+    cwd: __dirname,
+    stdio: "inherit",
+  });
+}
+
+async function prepareDatabase() {
+  if (!dev) return;
+  console.log("Migrating db..");
+  await spawnAsync("yarn", ["prisma", "migrate", "dev", "--preview-feature", "--skip-generate"], {
+    cwd: __dirname,
+    stdio: "inherit",
+    env: {
+      DATABASE_URL: databaseUrl,
+      ...process.env,
+    },
+  });
+}
+
+async function runServer() {
+  await prepareDockerDev();
+  await prepareDatabase();
+  await app.prepare();
+  await startServer();
+}
+
+runServer().catch((err) => {
+  console.error(err);
+});
+
+process.on("SIGHUP", () => {
+  console.info("SIGHUP signal received.");
+});
+
+process.on("SIGTERM", () => {
+  console.info("SIGTERM signal received.");
+  (async () => {
+    writeEvent("HostClose", undefined);
+    await flushEvents();
+    if (closeServer) await closeServer();
+    database.$disconnect();
+    await disconnectNotifications();
+  })()
+    .then(() => {
+      process.exit(0);
+    })
+    .catch((e) => {
+      console.error("Error while shutting down server");
+      // good luck finding this error in the production logs, lol!
+      console.error(e);
+      process.exit(1);
+    });
+});
